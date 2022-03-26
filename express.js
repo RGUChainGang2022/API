2e769c344d8ba692e422b65d3b36c0306e101457
Daniel <sandboxorigin@protonmail.com>
2022-03-26 23:02:36 +0000

    Added 404 system

--- express.js
+++ express.js
@@ -1,9 +1,19 @@
 import express from "express";
 import GetNGData from "./index.js";
+
+import path from "path";
+
+//Express setup
 const app = express();
 
+//Express routes
 app.get("/api/v1", (req, res)=>{
+    console.log(req.query);
     GetNGData(res);
+})
+
+app.get("/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, '/404.html'));
 })
 
 app.listen(1800, ()=>{
